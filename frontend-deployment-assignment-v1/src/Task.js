c8807c7e05504262ad3d9e4c19f87e8b1b6db512
didishaldyr <51758693+didishaldyr@users.noreply.github.com>
2022-12-15 00:24:02 +0700

    Delete frontend-deployment-assignment-v1 directory

--- frontend-deployment-assignment-v1/src/Task.js
+++
@@ -1,5 +0,0 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
-
-export { url, studentName, studentId }
